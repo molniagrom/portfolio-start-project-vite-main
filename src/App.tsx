d0229758cb9e76feaa32a8f0f6c045040cabf151
molnia_grom <mgrom250@gmail.com>
2025-05-15 17:40:36 +0300

    make Slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
-import {Header} from "./layout/header/Header.tsx";
-import Main from "./layout/sections/main/Main.tsx";
-import {Footer} from "./layout/footer/Footer.tsx";
-import {Projects} from "./layout/sections/Projects/Projects.tsx";
-import {AboutMe} from "./layout/sections/aboutMe/AboutMe.tsx";
-import {Highlights} from "./layout/sections/highlights/Highlights.tsx";
-import {MyStack} from "./layout/sections/myStack/MyStack.tsx";
+// import {Header} from "./layout/header/Header.tsx";
+// import Main from "./layout/sections/main/Main.tsx";
+// import {Footer} from "./layout/footer/Footer.tsx";
+// import {Projects} from "./layout/sections/Projects/Projects.tsx";
+// import {AboutMe} from "./layout/sections/aboutMe/AboutMe.tsx";
+// import {Highlights} from "./layout/sections/highlights/Highlights.tsx";
+// import {MyStack} from "./layout/sections/myStack/MyStack.tsx";
 import {Testimony} from "./layout/sections/testimony/Testimony.tsx";
-import {Quote} from "./layout/sections/quote/Quote.tsx";
-import {Contact} from "./layout/sections/contact/Contact.tsx";
+// import {Quote} from "./layout/sections/quote/Quote.tsx";
+// import {Contact} from "./layout/sections/contact/Contact.tsx";
 
 
 // import styled from "styled-components";
@@ -16,16 +16,16 @@ function App() {
 
     return (
         <div className="App">
-            <Header/>
-            <Main/>
-            <Projects/>
-            <AboutMe/>
-            <Highlights/>
-            <MyStack/>
+            {/*<Header/>*/}
+            {/*<Main/>*/}
+            {/*<Projects/>*/}
+            {/*<AboutMe/>*/}
+            {/*<Highlights/>*/}
+            {/*<MyStack/>*/}
             <Testimony/>
-            <Quote/>
-            <Contact/>
-            <Footer/>
+            {/*<Quote/>*/}
+            {/*<Contact/>*/}
+            {/*<Footer/>*/}
         </div>
     )
 }
--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,26 +1,105 @@
-
+// import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import styled from "styled-components";
-import {RatingStar} from 'react-ts-rating-star'
 import {theme} from "../../styles/Theme.ts";
 import {quote} from "../../image/svgDataFormat.ts";
 
-export const Slider = () => {
-    return (
-        <SliderStyled>
-            <Slide>
-                <ImageCard
-                    src="https://www.mckinsey.com/it/~/media/mckinsey/our%20people/alessandro%20agosta/alessandro-agosta_fc_mask_profile_1536x1152.jpg?cq=50&mw=480&car=1:1&cpy=Center"
-                    alt="person"/>
-                <SliderTitle>Adams Ademola</SliderTitle>
-                <SliderSubTitle>Happy Client</SliderSubTitle>
-                <Testimonial>“Everyone working in the office is very knowledgeable about all types of dental work and
-                    options for your individual needs.”</Testimonial>
-                <RatingStar numberOfStars={5}/>
-            </Slide>
-        </SliderStyled>
-    );
-};
+type SlideComponentPropsType = {
+    src: string,
+    alt: string,
+    name: string,
+    testimonial: string,
+    ratingStar: number,
+}
 
+const testimonials: SlideComponentPropsType[] = [
+    {
+        src: "https://www.mckinsey.com/it/~/media/mckinsey/our%20people/alessandro%20agosta/alessandro-agosta_fc_mask_profile_1536x1152.jpg?cq=50&mw=480&car=1:1&cpy=Center",
+        alt: "Adams Ademola",
+        name: "Adams Ademola",
+        testimonial: `Everyone working in the office is very knowledgeable about all types of dental work and
+options for your individual needs.`,
+        ratingStar: 5,
+    },
+    {
+        src: "https://randomuser.me/api/portraits/women/44.jpg",
+        alt: "Jessica Liu",
+        name: "Jessica Liu",
+        testimonial: `I was nervous at first, but the staff made me feel so comfortable. 
+They explained everything clearly and treated me with such kindness.`,
+        ratingStar: 5,
+    },
+    {
+        src: "https://randomuser.me/api/portraits/men/32.jpg",
+        alt: "Carlos Méndez",
+        name: "Carlos Méndez",
+        testimonial: `The clinic is modern and clean, and they use the latest technology. 
+I highly recommend their services to anyone who wants professional care.`,
+        ratingStar: 5,
+    },
+];
+
+
+const SlideComponent = (props: SlideComponentPropsType) => {
+    return (
+        <Slide>
+            <ImageCard
+                src={props.src}
+                alt={props.alt}/>
+            <SliderTitle>{props.name}</SliderTitle>
+            <SliderSubTitle>Happy Client</SliderSubTitle>
+            <Testimonial>“{props.testimonial}”</Testimonial>
+            {/*<Rating*/}
+            {/*    name="read-only"*/}
+            {/*    value={props.ratingStar}*/}
+            {/*    readOnly*/}
+            {/*    precision={1}*/}
+            {/*/>*/}
+
+        </Slide>
+    )
+}
+
+const items = testimonials.map((testimonial, index) => (
+    <SlideComponent
+        key={index}
+        {...testimonial}
+    />
+));
+
+export const Slider = () => (
+    <SliderStyled>
+        <AliceCarousel
+            mouseTracking
+            items={items}
+        />
+    </SliderStyled>
+);
+
+
+// import styled from "styled-components";
+// import {RatingStar} from 'react-ts-rating-star'
+// import {theme} from "../../styles/Theme.ts";
+// import {quote} from "../../image/svgDataFormat.ts";
+//
+// export const Slider = () => {
+//     return (
+//         <SliderStyled>
+//             <Slide>
+//                 <ImageCard
+//                     src="https://www.mckinsey.com/it/~/media/mckinsey/our%20people/alessandro%20agosta/alessandro-agosta_fc_mask_profile_1536x1152.jpg?cq=50&mw=480&car=1:1&cpy=Center"
+//                     alt="person"/>
+//                 <SliderTitle>Adams Ademola</SliderTitle>
+//                 <SliderSubTitle>Happy Client</SliderSubTitle>
+//                 <Testimonial>“Everyone working in the office is very knowledgeable about all types of dental work and
+//                     options for your individual needs.”</Testimonial>
+//                 <RatingStar numberOfStars={5}/>
+//             </Slide>
+//         </SliderStyled>
+//     );
+// };
+//
 const SliderStyled = styled.div`
     background-color: ${theme.colors.allBg};
     width: 60%;
